8877a2084e74b61839a5e798a84fdda7b89060ef
Laura Daugherty <lauradaugherty6@gmail.com>
2020-03-27 08:22:14 -0700

    sprint challenge solution

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,19 +1,7 @@
 """CPU functionality."""
 
 import sys
-LDI = "10000010"
-PRN = "01000111"
-HLT = "00000001"
-MUL = "10100010"
-ADD = "10100000"
-PUSH = "01000101"
-POP = "01000110"
-CALL = "01010000"
-RET = "00010001"
-JMP = "01010100"
-CMP = "10100111"
-JEQ = "01010101"
-JNE = "01010110"
+from opcode import *
 class CPU:
     """Main CPU class."""
     def __init__(self):
@@ -23,7 +11,113 @@ class CPU:
         self.pc = 0
         self.sp = 7
         self.fl = "00000000"
+        self.branchtable = {}
+        self.branchtable[LDI] = self.handle_LDI
+        self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[HLT] = self.handle_HLT
+        self.branchtable[MUL] = self.handle_MUL
+        self.branchtable[ADD] = self.handle_ADD
+        self.branchtable[PUSH] = self.handle_PUSH
+        self.branchtable[POP] = self.handle_POP
+        self.branchtable[CALL] = self.handle_CALL
+        self.branchtable[RET] = self.handle_RET
+        self.branchtable[JMP] = self.handle_JMP
+        self.branchtable[CMP] = self.handle_CMP
+        self.branchtable[JEQ] = self.handle_JEQ
+        self.branchtable[JNE] = self.handle_JNE
 
+    
+    def handle_LDI(self, ir, operand_a, operand_b):
+        print("writing")
+        self.raw_write(operand_b, operand_a)
+        self.calculate_pc(ir)
+    def handle_PRN(self, ir, operand_a):
+        print(self.reg[operand_a])
+        self.calculate_pc(ir)
+    def handle_HLT(self):
+        sys.exit(1)
+    def handle_MUL(self, op, reg_a, reg_b):
+        self.reg[reg_a] *= self.reg[reg_b]
+        self.calculate_pc(op)    
+    def handle_ADD(self, op, reg_a, reg_b):
+        self.reg[reg_a] += self.reg[reg_b]
+        self.calculate_pc(op)    
+    def handle_PUSH(self, ir):
+        # print("register", self.reg)
+        # print("ram", self.ram)
+        reg = self.ram[self.pc + 1]
+        # print("push reg", reg)
+        val = self.reg[int(reg)]
+        # print("push val", val)
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = val
+        self.calculate_pc(ir)
+    def handle_POP(self, ir):
+        # print("register", self.reg)
+        # print("ram", self.ram)
+        reg = self.ram[self.pc + 1]
+        # print("reg", reg)
+        val = self.ram[self.reg[self.sp]]
+        # print("val", val)
+        # print("int reg", int(reg, 2))
+        self.reg[int(reg, 2) ] = val
+        self.reg[self.sp] += 1
+        self.calculate_pc(ir)
+    def handle_CALL(self):
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = self.pc + 2
+        reg = self.ram[self.pc + 1]
+        self.pc = self.reg[int(reg, 2)]    
+    def handle_RET(self):
+        self.pc = self.ram[self.reg[self.sp]]
+        self.reg[self.sp] += 1    
+    def handle_JMP(self):
+        reg = self.ram[self.pc + 1]
+        self.pc = self.reg[int(reg, 2)]
+    def handle_CMP(self, op, reg_a, reg_b):
+        # Compare the values in two registers.
+        # * If they are equal,
+        if self.reg[reg_a] == self.reg[reg_b]:
+        #   set the Equal `E` flag to 1, 
+            self.fl = "00000001"
+            self.calculate_pc(op)
+            print("self.fl", self.fl)
+        # * If registerA is less than registerB, 
+        #   set the Less-than `L` flag to 1,
+        # otherwise 
+        #   set it to 0.
+        elif self.reg[reg_a] < self.reg[reg_b]:
+            self.fl = "00000100"
+            self.calculate_pc(op)
+            print("self.fl", self.fl)
+#             * If registerA is greater than registerB, 
+#                 set the Greater-than `G` flag
+#               to 1, otherwise set it to 0
+        elif self.reg[reg_a] > self.reg[reg_b]:
+            self.fl = "00000010"
+            self.calculate_pc(op)
+            print("self.fl", self.fl)
+        else:
+            self.fl = "00000000"
+            self.calculate_pc(op)
+            print("self.fl", self.fl)
+    def handle_JEQ(self, ir):
+        if self.fl[-1] == "1":
+            reg = self.ram[self.pc + 1]
+            # print(reg)
+            self.pc = self.reg[int(reg, 2)]
+            # print(self.reg)
+        else:
+            self.calculate_pc(ir)    
+    def handle_JNE(self, ir):
+        if self.fl[-1] == "0":
+            reg = self.ram[self.pc + 1]
+            self.pc = self.reg[int(reg, 2)]
+            # print(self.reg)
+        else:
+            self.calculate_pc(ir)
+        #If `E` flag is clear (false, 0), 
+            # jump to the address stored in the given register.
 
     def load(self, filename):
     # def load(self):
@@ -31,7 +125,6 @@ class CPU:
         try:
             address = 0
             with open(filename) as f:
-
                 #read all the lines
                 for line in f:
                     #parse out comments
@@ -48,67 +141,19 @@ class CPU:
             print("File not Found")
             sys.exit(2)
 
-        # For now, we've just hardcoded a program:
-        # address = 0
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
+    def calculate_pc(self, op):
+        self.pc += (int(op, 2) >> 6) + 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == ADD:
-            self.reg[reg_a] += self.reg[reg_b]
-            self.pc += 3
-            # print(self.reg)
-        #elif op == "SUB": etc
-
+            self.branchtable[op](op, reg_a, reg_b)
         elif op == MUL:
             print("multiplying in ALU")
-            self.reg[reg_a] *= self.reg[reg_b]
-            self.pc += 3
+            self.branchtable[op](op, reg_a, reg_b)
         elif op == CMP:
             print("CMP in ALU")
-            # Compare the values in two registers.
-            # * If they are equal,
-            if self.reg[reg_a] == self.reg[reg_b]:
-            #   set the Equal `E` flag to 1, 
-                self.fl = "00000001"
-                self.pc +=3
-                print("self.fl", self.fl)
-            # * If registerA is less than registerB, 
-            #   set the Less-than `L` flag to 1,
-            # otherwise 
-            #   set it to 0.
-            elif self.reg[reg_a] < self.reg[reg_b]:
-                self.fl = "00000100"
-                self.pc +=3
-                print("self.fl", self.fl)
-
-#             * If registerA is greater than registerB, 
-#                 set the Greater-than `G` flag
-#               to 1, otherwise set it to 0
-            elif self.reg[reg_a] > self.reg[reg_b]:
-                self.fl = "00000010"
-                self.pc +=3
-                print("self.fl", self.fl)
-
-            else:
-                self.fl = "00000000"
-                self.pc +=3
-                print("self.fl", self.fl)
-
+            self.branchtable[op](op, reg_a, reg_b)
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -150,87 +195,49 @@ class CPU:
             # print("LDI type", type(LDI))
             operand_a = int(str(self.ram_read(self.pc + 1)), 2)
             operand_b = int(str(self.ram_read(self.pc + 2)), 2)
+            print("op A", operand_a)
+            print("op B", operand_b)
             if ir == LDI:
                 print("writing")
-                self.raw_write(operand_b, operand_a)
-                self.pc += 3
+                self.branchtable[ir](ir, operand_a, operand_b)
             elif ir == PRN:
                 print("printing")
-                # print("opA", operand_a)
-                print(self.reg[operand_a])
-                self.pc += 2
+                self.branchtable[ir](ir, operand_a)
             elif ir == PUSH:
                 print("pushing")
-                # print("register", self.reg)
-                # print("ram", self.ram)
-                reg = self.ram[self.pc + 1]
-                # print("push reg", reg)
-                val = self.reg[int(reg)]
-                # print("push val", val)
-                self.reg[self.sp] -= 1
-                self.ram[self.reg[self.sp]] = val
-                self.pc +=2
+                self.branchtable[ir](ir)
             elif ir == POP:
                 print("popping")
-                # print("register", self.reg)
-                # print("ram", self.ram)
-                reg = self.ram[self.pc + 1]
-                # print("reg", reg)
-                val = self.ram[self.reg[self.sp]]
-                # print("val", val)
-                # print("int reg", int(reg, 2))
-                self.reg[int(reg, 2) ] = val
-                self.reg[self.sp] += 1
-                self.pc += 2
+                self.branchtable[ir](ir)
             elif ir == CALL:
                 print("calling")
-                self.reg[self.sp] -= 1
-                self.ram[self.reg[self.sp]] = self.pc + 2
-                reg = self.ram[self.pc + 1]
-                self.pc = self.reg[int(reg, 2)]
+                self.branchtable[ir]()
             elif ir == CMP:
                 print("CMP")
                 self.alu(ir, operand_a, operand_b)
             elif ir == JMP:
                 print("jumping")
-                # self.reg[self.sp] -= 1
-                # self.ram[self.reg[self.sp]] = self.pc + 2
-                reg = self.ram[self.pc + 1]
-                self.pc = self.reg[int(reg, 2)]
+                self.branchtable[ir]()
             elif ir == JEQ:
-                # print(self.fl[-1])
                 print("JEQ")
-                if self.fl[-1] == "1":
-                    reg = self.ram[self.pc + 1]
-                    # print(reg)
-                    self.pc = self.reg[int(reg, 2)]
-                    # print(self.reg)
-                else:
-                    self.pc += 2
-
-                # print("JEQ register")
-                # If `equal` flag is set (true), 
-                    # jump to the address stored in the given register.
+                self.branchtable[ir](ir)
             elif ir == JNE:
                 print("JNE register")
-
-                if self.fl[-1] == "0":
-                    reg = self.ram[self.pc + 1]
-                    self.pc = self.reg[int(reg, 2)]
-                    # print(self.reg)
-                else:
-                    self.pc += 2
-                #If `E` flag is clear (false, 0), 
-                    # jump to the address stored in the given register.
-
+                self.branchtable[ir](ir)
             elif ir == RET:
                 print("returning")
-                self.pc = self.ram[self.reg[self.sp]]
-                self.reg[self.sp] += 1
+                self.branchtable[ir]()
             elif ir == MUL:
                 self.alu(ir, operand_a, operand_b)
             elif ir == ADD:
                 self.alu(ir, operand_a, operand_b)
             elif ir == HLT:
                 print("halting")
-                sys.exit(1)
+                self.branchtable[ir]()
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: file.py filename", file=sys.stderr)
+        sys.exit(1)
+    cpu = CPU()
+    cpu.load(sys.argv[1])
+    cpu.run()
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -6,6 +6,7 @@ import sys
 from cpu import *
 
 cpu = CPU()
+print(sys.argv)
 cpu.load(sys.argv[1])
 # cpu.load()
 
